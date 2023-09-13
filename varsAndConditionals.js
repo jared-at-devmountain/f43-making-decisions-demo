fc46c96bc77a3aac01e41d663d805c78be5745d2
Jared Collier <collierjared@gmail.com>
2023-09-13 13:06:12 -0600

    adds jamie attack

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -15,5 +15,12 @@ if (jonSnowAttack > jamieLannisterAttack) {
     console.log('Our fighters are evenly matched')
 }
 
+let jonSnowHealth = 100
+let jonSnowDefense = 0
 
-
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log('Jon Snow has been slain.')
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
+}
